7681b46ae902c160d15223e8af368ca63f2da802
liquidberny <bernardo.urquijo.bu@gmail.com>
2023-10-11 12:51:32 -0700

    loading y carga de cosas

--- src/events/script.js
+++ src/events/script.js
@@ -81,29 +81,14 @@ const evento6 = new eventos(
 
 const eventosList = [evento1, evento2, evento3, evento4, evento5, evento6];
 
-// ordernar por indices
 
-console.log("Impresion en consola de elementos accesados por indices: ");
-console.log(eventosList[0]);
-console.log(eventosList[1]);
-console.log(eventosList[2]);
-
-// Accedemos datos con funcion forEach() de array
-console.log("Impresion en consola de elementos accesados con forEach(): ");
-eventosList.forEach((item) => {
-  console.log(item);
-});
-
-// Funcion que controla el despliegue de un array de RealEstate en la tabla, asi como el mensaje a mostrar.
-function displayTable(eventos) {
-
-  clearTable();
+function displayTable(houses) {
 
   showLoadingMessage();
 
   setTimeout(() => {
 
-    if (eventos.length === 0) {
+    if (houses.length === 0) {
 
       showNotFoundMessage();
 
@@ -111,27 +96,30 @@ function displayTable(eventos) {
 
         hideMessage();
 
-        const tablaBody = document.getElementById('data-table-body');
+        const eventContainer = document.querySelector(".event-list");
 
-        const imagePath = `../assets/img/events`;
+        const imagePath = `../assets/img/events/`;
 
-        eventos.forEach(eventos => {
-
-          const row = document.createElement('tr');
-
-          row.innerHTML = `
-            <td> ${eventos.id} </td>
-            <td> <img src="${imagePath + eventos.image}" alt="${eventos.name}" width="100"> </td>
-            <td>${eventos.name}</td>
-            <td>${eventos.description}</td>
-            <td>${eventos.fecha}</td>
-            <td>${eventos.lugar}</td>
-            <td>${eventos.pais}</td>
-            <td>${formatCurrency(eventos.price)}</td>
+        eventosList.forEach((evento) => {
+          const eventDiv = document.createElement("div");
+          eventDiv.classList.add("event");
+          
+          eventDiv.innerHTML = `
+            <div class="event-image">
+              <img src="${imagePath+evento.image}" alt="${evento.name}" />
+            </div>
+            <div class="event-details">
+              <div class="event-info">
+                <p><strong>Nombre:</strong> ${evento.name}</p>
+                <p><strong>Ubicación:</strong> ${evento.lugar}, ${evento.pais}</p>
+                <p><strong>Precio:</strong> $${evento.precio}</p>
+                <p><strong>Fecha:</strong> ${evento.fecha}</p>
+                <p><strong>Descripción:</strong> ${evento.description}</p>
+              </div>
+            </div>
           `;
-
-          tablaBody.appendChild(row);
-
+      
+          eventContainer.appendChild(eventDiv);
         });
 
     }
@@ -140,109 +128,29 @@ function displayTable(eventos) {
 
 }
 
-
-const eventContainer = document.querySelector(".event-list");
-const imagePath = `../assets/img/events/`;
-
-document.addEventListener("DOMContentLoaded", function () {
-  const eventContainer = document.querySelector(".event-list");
-
-  eventosList.forEach((evento) => {
-    const eventDiv = document.createElement("div");
-    eventDiv.classList.add("event");
-    eventDiv.innerHTML = `
-      <div class="event-image">
-        <img src="${imagePath+evento.image}" alt="${evento.name}" />
-      </div>
-      <div class="event-details">
-        <div class="event-info">
-          <p><strong>Nombre:</strong> ${evento.name}</p>
-          <p><strong>Ubicación:</strong> ${evento.lugar}, ${evento.pais}</p>
-          <p><strong>Precio:</strong> $${evento.precio}</p>
-          <p><strong>Fecha:</strong> ${evento.fecha}</p>
-          <p><strong>Descripción:</strong> ${evento.description}</p>
-        </div>
-      </div>
-    `;
-
-    eventContainer.appendChild(eventDiv);
-  });
-});
-
-// funcion para limpiar tabla no estoy seguro si es necesario
-
-function clearTable() {
-  const tableBody = document.getElementById('data-table-body');
-
-  tableBody.innerHTML = '';
-}
-
-
-// funcion muestra mensaje de carga
-
+// Funcion que muestra mensaje de carga
 function showLoadingMessage() {
-  const message = document.getElementById('message');
-
-  message.innerHTML = 'Espera jeje cargando...';
+  const message = document.querySelector('.message');
+  console.log(message);
+  message.innerHTML = 'Cargando...';
 
   message.style.display = 'block';
 }
 
 // Funcion que muestra mensaje de que no se encuentraron datos
 function showNotFoundMessage() {
-  const message = document.getElementById('message');
+  const message = document.querySelector('.message');
 
-  message.innerHTML = 'No hay eventos con tus filtros especificos.';
+  message.innerHTML = 'No se encontraron casas con el filtro proporcionado.';
 
   message.style.display = 'block';
 }
 
 // Funcion que oculta mensaje
 function hideMessage() {
-  const message = document.getElementById('message');
+  const message = document.querySelector('.message');
 
   message.style.display = 'none';
 }
 
-// Funcion que inicializa los eventos de los botones del filto
-function initButtonsHandler() {
-
-  document.getElementById('filter-form').addEventListener('submit', event => {
-    event.preventDefault();
-    applyFilters();
-  });
-
-  document.getElementById('reset-filters').addEventListener('click', () => {
-    document.querySelectorAll('input.filter-field').forEach(input => input.value = '');
-    applyFilters();
-  });
-
-}
-
-// Funcion que gestiona la aplicacion del filtro a los datos y su despliegue.
-function applyFilters() {
-  const filterText = document.getElementById('text').value.toLowerCase();;
-  const filterPais = parseFloat(document.getElementById('pais').value);
-  const filterMinPrice = parseFloat(document.getElementById('price-min').value);
-  const filterMaxPrice = parseFloat(document.getElementById('price-max').value);
-
-  const filteredEventos = filterEventos(eventosList, filterText, filterPais, filterMinPrice, filterMaxPrice);
-
-  displayTable(filteredEventos);
-}
-
-// Funcion con la logica para filtrar los eventos.
-function filterEventos(eventos, text, pais, minPrice, maxPrice) {
-
-  return eventos.filter( eventos =>
-      (!pais || eventos.pais === pais) &&
-      (!minPrice || eventos.price >= minPrice) &&
-      (!maxPrice || eventos.price <= maxPrice) &&
-      (!text     || eventos.name.toLowerCase().includes(text) || eventos.description.toLowerCase().includes(text))
-    );
-}
-
-
 displayTable(eventosList);
-
-initButtonsHandler();
